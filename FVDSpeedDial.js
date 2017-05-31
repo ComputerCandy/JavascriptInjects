ef2ad9b1ab63e7e6894081d1db7acb99afee8bcf
ComputerCandy <thomas.kool.12@gmail.com>
2017-05-31 14:21:07 +1200

    Added headers and javascript source
    ugh ask me l8r

---
+++ FVDSpeedDial.js
@@ -0,0 +1,7 @@
+/*****************************************\
+| Extention: FVD Speed Dial               |
+| How To use: Open Inspect element and    |
+| paste in the code                       |
+| Author: Thomas Hobson (ComputerCandy)   |
+\*****************************************/
+Aes.Ctr.decrypt(fvdSpeedDial.Prefs.get("poweroff.password"), "*j12398sdfh4123iud9123", 256);
